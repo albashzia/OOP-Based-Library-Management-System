8bbf5c31fd9762c374ba613af4aeee1519740569
albashzia <ahmedalbashzia@gmail.com>
2026-03-13 19:28:33 +0500

    Adding method headers for Book related methods to Library class

--- Code/src/Library.java
+++ Code/src/Library.java
@@ -1,2 +1,17 @@
-public class Library {
+public class Library
+{
+    void addBook() {
+    }
+
+    void removeBook() {
+    }
+
+    void displayAllBooks() {
+    }
+
+    void issueBook() {
+    }
+
+    void returnBook() {
+    }
 }
