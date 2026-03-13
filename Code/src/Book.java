c32614213b974bc174821104802f505f393c725b
albashzia <ahmedalbashzia@gmail.com>
2026-03-13 19:30:19 +0500

    Adding a parameterized constructor for Book objects of Book class

--- Code/src/Book.java
+++ Code/src/Book.java
@@ -4,4 +4,14 @@ public class Book {
     String author;
     int totalQuantity;
     int availableQuantity;
+
+    Book(int id, String title, String author, int qty) {
+
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.totalQuantity = qty;
+        this.availableQuantity = qty;
+    }
+
 }
