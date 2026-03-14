22044363fc4b9ab1dc1137051253e2050f199d32
asra-06 <waziralamasra@gmail.com>
2026-03-14 19:58:15 +0500

    Implemented Person and Member class with inheritance and core Methods

--- Code/src/Member.java
+++ Code/src/Member.java
@@ -1,2 +1,43 @@
-public class Member {
+public class Member extends Person {
+    private String phone;
+    private String email;
+
+    //default constructor
+    public Member(){
+        this.phone = "xxxxxxxxxxx";
+        this.email = "----@gmail.com";
+    }
+
+    //parameterized constructor
+    public Member(String id, String name, String phoneNumber, String mail){
+        super(id,name);
+        this.phone = phoneNumber;
+        this.email = mail;
+    }
+
+    //setters
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    //getters
+    public String getPhone(){
+        return phone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    //display method
+    public void displayMemberDetails(){
+        super.displayDetails();
+        System.out.println("Phone Number: " +this.phone);
+        System.out.println("Email: " +this.email);
+    }
+
 }
---
+++ Code/src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String id;
+    private String name;
+
+    //default constructor
+    public Person(){
+        id = "null";
+        name = "none";
+    }
+
+    //parameterized constructor
+    public Person(String i, String n){
+        id = i;
+        name = n;
+    }
+
+    //setters
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    //getters
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //display method
+    public void displayDetails(){
+        System.out.println("ID: " +this.id);
+        System.out.println("Name: " +this.name);
+    }
+}
