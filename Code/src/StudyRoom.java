2ce760142072a6c55349dd28de2e73bbe298c26f
albashzia <ahmedalbashzia@gmail.com>
2026-03-15 15:35:25 +0500

    Defining constructors in StudyRoom class

--- Code/src/StudyRoom.java
+++ Code/src/StudyRoom.java
@@ -2,4 +2,16 @@ public class StudyRoom {
     private int roomNumber;
     private boolean isReserved;
     private int reservedByMemberID;
+
+    StudyRoom(){
+        roomNumber=0;
+        isReserved=false;
+        reservedByMemberID=0;
+    }
+    StudyRoom(int roomNumber,boolean isReserved,int reservedByMemberID){
+        this.roomNumber=roomNumber;
+        this.isReserved=isReserved;
+        this.reservedByMemberID=reservedByMemberID;
+    }
+
 }
